326be98e9f1df58b9a09ea885b7f835f86b928ce
Sahar Jafari <shr.jafari@gmail.com>
2020-09-30 18:02:34 -0700

    custom hook for darkMode

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = (key, initialValues, cb) => {
+  const [values, setValues] = useLocalStorage(key, initialValues);
+
+  const setDarkMode = (v) => {
+    setValues(v);
+  };
+  return [values, setDarkMode];
+};
+
+export default useDarkMode;
